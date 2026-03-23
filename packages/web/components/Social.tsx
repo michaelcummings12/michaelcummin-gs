354fede157111be2a65ba7ed7e47009dbe5d4f4d
Michael Cummings <cummingsdmichael@gmail.com>
2026-03-23 11:37:11 -0600

    Fix social link

--- packages/web/components/Social.tsx
+++ packages/web/components/Social.tsx
@@ -5,7 +5,7 @@ import { Envelope, Github, Instagram, LinkedIn } from "./Icons";
 const Social: FunctionComponent = () => {
 	return (
 		<div className="flex flex-row gap-4">
-			<Link href="https://github.com/michaelcummings12" className="rounded-xl bg-black p-2 shadow-md transition-all hover:scale-105 active:scale-95">
+			<Link href="https://instagram.com/michaelcummingsofficial" className="rounded-xl bg-black p-2 shadow-md transition-all hover:scale-105 active:scale-95">
 				<Instagram className="fill-white" height="32px" />
 			</Link>
 			<Link href="https://linkedin.com/in/michael-cummings-21b526124" className="rounded-xl bg-black p-2 shadow-md transition-all hover:scale-105 active:scale-95">
